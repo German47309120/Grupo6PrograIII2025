1eb8c6f822bfeb3c4197c7c05035e0c65698538c
Renato Escobar <>
2025-03-11 21:58:19 -0600

    Primer cambio Renato

---
+++ ExcelGrupo6/src/main/java/com/mycompany/excelgrupo6/ExcelGrupo6.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.excelgrupo6;
+
+/**
+ *
+ * @author s u s
+ */
+public class ExcelGrupo6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int a=10;
+        int b=5;
+        int c= a+b;
+        
+        System.out.println("total: " +c);
+    }
+}
